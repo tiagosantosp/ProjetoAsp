ea855b7b2c44afdc2ad14563056ca7073d8a6f24
Tiago <tiago_s.p@hotmail.com>
2023-02-07 07:28:47 -0300

    Adicionando o SellerService.cs

---
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+
+        private readonly SellerService _sellerService;
+
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
+        public IActionResult Index()
+        {
+            var list = _sellerService.FindAll();
+
+            return View(list);
+        }
+    }
+}
